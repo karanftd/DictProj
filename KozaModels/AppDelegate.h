7450dd476ca5f650a1e837b1ed93bb403a425357
Karan Patel <karan.bhalodiya@gmail.com>
2015-02-17 21:08:13 +0530

    Initial Commit

---
+++ KozaModels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KozaModels
+//
+//  Created by Karan Patel on 17/02/15.
+//  Copyright (c) 2015 Koaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
