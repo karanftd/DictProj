225e3a9953bc21127c360ea7ae4fc6c049249b48
Karan Patel <karan.bhalodiya@gmail.com>
2015-03-05 16:55:56 +0530

    Added Pod

---
+++ KozaModels/ViewControllers/LeftSide/MenuVC.h
@@ -0,0 +1,13 @@
+//
+//  MenuVC.h
+//  KozaModels
+//
+//  Created by Karan Patel on 03/03/15.
+//  Copyright (c) 2015 Koaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuVC : UIViewController
+
+@end
